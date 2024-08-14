c5c1f40e8c053b4c9dabedf6790c31e75f700fec
Mamefatou05 <fatouniangbasse@gmail.com>
2024-08-14 18:54:26 +0000

    delte .js

--- src/utils/tokenUtils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateToken = void 0;
-const generateToken = (payload) => {
-    // return jwt.sign(payload, JWT_SECRET, { expiresIn: '1d' });
-};
-exports.generateToken = generateToken;
