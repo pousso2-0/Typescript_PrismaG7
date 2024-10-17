b19b7e12b3738701a9413ed3f6c4c671529c7960
avatarodc <odcmamadougueye@gmail.com>
2024-10-17 12:12:18 +0000

    notification

--- prisma/migrations/20241016152322_modify_store_model/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX "Store_userId_key";
-
--- RenameForeignKey
-ALTER TABLE "View" RENAME CONSTRAINT "FK_View_StatusViewer" TO "FK_View_User";
-
--- RenameForeignKey
-ALTER TABLE "View" RENAME CONSTRAINT "FK_View_User" TO "FK_View_StatusViewer";
