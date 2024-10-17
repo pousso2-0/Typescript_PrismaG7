aa225f18f934415534f2f00e30e4894d2eff3804
MAMADOU GIUEYE <162546167+avatarodc@users.noreply.github.com>
2024-10-17 13:52:32 +0000

    Update swagger.ts

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -12,7 +12,7 @@ const options = {
     },
     servers: [
       {
-        url: `https://tailor-0722.onrender.com`,
+        url: `https://typescript-prismag7.onrender.com`,
         description: 'Serveur de développement',
       },
       {
